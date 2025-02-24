9c57168c7309f57c9e0ceff45fee6ae987ea6af1
joyplustravelapp <joyplustravelapp@gmail.com>
2025-02-24 23:10:36 +0100

    Update Application class to include ViewModelModule

--- androidApp/src/main/kotlin/com/joyplus/travelapp/android/JoyPlusTravelApp.kt
+++ androidApp/src/main/kotlin/com/joyplus/travelapp/android/JoyPlusTravelApp.kt
@@ -1,9 +1,12 @@
 package com.joyplus.travelapp.android
 
 import android.app.Application
+import com.joyplus.travelapp.android.di.viewModelModule
 import com.joyplus.travelapp.db.AndroidDatabaseDriverFactory
 import com.joyplus.travelapp.di.initKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
 import org.koin.dsl.module
 
 /**
@@ -19,6 +22,11 @@ class JoyPlusTravelApp : Application() {
         initKoin(
             module {
                 single { AndroidDatabaseDriverFactory(androidContext()) }
+            },
+            androidModule = {
+                androidLogger(Level.ERROR) // Use ERROR level to avoid Koin Android logger issues
+                androidContext(this@JoyPlusTravelApp)
+                modules(viewModelModule)
             }
         )
     }
