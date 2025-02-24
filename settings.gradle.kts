092eaa6caae0eab12f0c54b3e981001a89ed2683
joyplustravelapp <joyplustravelapp@gmail.com>
2025-02-24 22:58:41 +0100

    Add Gradle settings file

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JoyPlusTravelApp"
+include(":shared")
+include(":androidApp")
