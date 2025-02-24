fe69b857b7cb15b87c773808939a33f105facdb9
joyplustravelapp <joyplustravelapp@gmail.com>
2025-02-24 22:59:10 +0100

    Add shared module build file

---
+++ shared/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("app.cash.sqldelight") version "2.0.0"
+}
+
+kotlin {
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("io.insert-koin:koin-core:3.4.3")
+                implementation("app.cash.sqldelight:coroutines-extensions:2.0.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("app.cash.sqldelight:android-driver:2.0.0")
+            }
+        }
+        val androidUnitTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("app.cash.sqldelight:native-driver:2.0.0")
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+android {
+    namespace = "com.joyplus.travelapp"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
+
+sqldelight {
+    databases {
+        create("JoyPlusTravelDatabase") {
+            packageName = "com.joyplus.travelapp.db"
+        }
+    }
+}
